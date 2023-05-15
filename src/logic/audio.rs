f71c0adc657e46ae53d6aa447f68694446c4361c
Konkitoman <konkitoman@users.noreply.github.com>
2023-05-15 19:55:53 +0300

    Fix typo

--- src/logic/audio.rs
+++ src/logic/audio.rs
@@ -6,11 +6,11 @@ use super::{
 impl TheManLogic {
     pub async fn on_audio_message(&mut self, message: Message) {
         match message {
-            Message::Audio(AudioMessage::ResCreateInputChannel(id, codec)) => {
-                println!("Audio created input: Id: {id}, Codec: {codec}");
+            Message::Audio(AudioMessage::ResCreateInputChannel(id, error)) => {
+                println!("Audio created input: Id: {id}, Error: {codec}");
             }
-            Message::Audio(AudioMessage::ResCreateOutputChannel(id, codec)) => {
-                println!("Audio created output: Id: {id}, Codec: {codec}");
+            Message::Audio(AudioMessage::ResCreateOutputChannel(id, error)) => {
+                println!("Audio created output: Id: {id}, Error: {codec}");
             }
             Message::Audio(AudioMessage::InputData { id, data }) => {
                 let _ = self
