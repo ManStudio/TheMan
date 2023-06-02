66e561081560fb9b387c765fc0435fbef70e3d52
Konkitoman <konkitoman@users.noreply.github.com>
2023-06-02 17:11:20 +0300

    Change opus library

--- src/audio/codec/opus/mod.rs
+++ src/audio/codec/opus/mod.rs
@@ -1,5 +1,5 @@
 use bytes_kman::TBytes;
-use opus::{Decoder, Encoder};
+use opus_kman::{Application, Decoder, Encoder, SampleRate, TDecoder, TEncoder};
 
 use super::Codec;
 
@@ -7,9 +7,8 @@ pub struct CodecOpus {
     errors: Vec<String>,
     pub decoder: Decoder,
     pub encoder: Encoder,
-    pub channels: opus::Channels,
-    pub application: opus::Application,
-    pub sample_rate: u32,
+    pub channels: u32,
+    pub application: opus_kman::Application,
     pub input_buffer: Vec<f32>,
     pub output_buffer: Vec<u8>,
     pub bitrate: i32,
@@ -19,11 +18,11 @@ impl Clone for CodecOpus {
     fn clone(&self) -> Self {
         let mut s = Self {
             errors: self.errors.clone(),
-            decoder: Decoder::new(self.sample_rate, self.channels).unwrap(),
-            encoder: Encoder::new(self.sample_rate, self.channels, self.application).unwrap(),
+            decoder: Decoder::new(SampleRate::Hz48000, self.channels).unwrap(),
+            encoder: Encoder::new(SampleRate::Hz48000, self.channels, self.application.clone())
+                .unwrap(),
             channels: self.channels,
-            application: self.application,
-            sample_rate: self.sample_rate,
+            application: self.application.clone(),
             input_buffer: self.input_buffer.clone(),
             output_buffer: self.output_buffer.clone(),
             bitrate: self.bitrate,
@@ -41,14 +40,12 @@ unsafe impl Sync for CodecOpus {}
 
 impl Default for CodecOpus {
     fn default() -> Self {
-        println!("Opus version: {}", opus::version());
         let mut s = Self {
             errors: Vec::new(),
-            decoder: Decoder::new(8000, opus::Channels::Mono).unwrap(),
-            encoder: Encoder::new(8000, opus::Channels::Mono, opus::Application::Voip).unwrap(),
-            channels: opus::Channels::Mono,
-            application: opus::Application::Voip,
-            sample_rate: 8000,
+            decoder: Decoder::new(SampleRate::Hz48000, 1).unwrap(),
+            encoder: Encoder::new(SampleRate::Hz48000, 1, Application::VOIP).unwrap(),
+            channels: 1,
+            application: Application::VOIP,
             input_buffer: Vec::new(),
             output_buffer: Vec::new(),
             bitrate: 128000,
@@ -63,21 +60,16 @@ impl Default for CodecOpus {
 
 impl CodecOpus {
     fn setup_buffers(&mut self) {
-        let channels = match self.channels {
-            opus::Channels::Mono => 1usize,
-            opus::Channels::Stereo => 2usize,
-        };
-
-        let len = self.sample_rate as usize * channels;
+        let len = 48000 * self.channels as usize;
         self.input_buffer.resize(len, 0.0);
         self.output_buffer.resize(len, 0);
     }
 
     fn setup(&mut self) {
-        self.encoder
-            .set_bitrate(opus::Bitrate::Bits(self.bitrate))
-            .unwrap();
-        let _ = self.encoder.set_inband_fec(true);
+        // self.encoder
+        //     .set_bitrate(opus::Bitrate::Bits(self.bitrate))
+        //     .unwrap();
+        // let _ = self.encoder.set_inband_fec(true);
     }
 }
 
@@ -93,14 +85,11 @@ impl Codec for CodecOpus {
     fn get_setting(&mut self, key: String) -> Option<the_man::Atom> {
         match key.trim() {
             "sample_rate" => Some(the_man::Atom::UnSigned {
-                value: self.sample_rate as usize,
-                range: u32::MIN as usize..u32::MAX as usize,
+                value: 48000,
+                range: 48000 as usize..48001 as usize,
             }),
             "channels" => Some(the_man::Atom::UnSigned {
-                value: match self.channels {
-                    opus::Channels::Mono => 1,
-                    opus::Channels::Stereo => 2,
-                },
+                value: self.channels as usize,
                 range: 1..3,
             }),
             _ => None,
@@ -114,50 +103,43 @@ impl Codec for CodecOpus {
         }
         match key.trim() {
             "sample_rate" => {
-                if let the_man::Atom::UnSigned { value, .. } = value {
-                    let new_decoder = Decoder::new(value as u32, self.channels.clone());
+                if let the_man::Atom::UnSigned { .. } = value {
+                    let new_decoder = Decoder::new(SampleRate::Hz48000, self.channels.clone());
                     let new_encoder = Encoder::new(
-                        value as u32,
+                        SampleRate::Hz48000,
                         self.channels.clone(),
                         self.application.clone(),
                     );
 
                     match new_decoder {
                         Ok(decoder) => self.decoder = decoder,
-                        Err(err) => self.errors.push(format!("OpusDecoder: Error: {err}")),
+                        Err(err) => self.errors.push(format!("OpusDecoder: Error: {err:?}")),
                     }
                     match new_encoder {
                         Ok(encoder) => self.encoder = encoder,
-                        Err(err) => self.errors.push(format!("OpusEncoder: Error: {err}")),
+                        Err(err) => self.errors.push(format!("OpusEncoder: Error: {err:?}")),
                     }
 
-                    self.sample_rate = value as u32;
                     self.setup_buffers();
                     self.setup();
                 }
             }
             "channels" => {
                 if let the_man::Atom::UnSigned { value, .. } = value {
-                    let channels = match value {
-                        1 => opus::Channels::Mono,
-                        2 => opus::Channels::Stereo,
-                        _ => {
-                            panic!("OpusCodec Invalid channels: {value}")
-                        }
-                    };
-                    let new_decoder = Decoder::new(self.sample_rate, channels);
-                    let new_encoder = Encoder::new(self.sample_rate, channels, self.application);
+                    let new_decoder = Decoder::new(SampleRate::Hz48000, value as u32);
+                    let new_encoder =
+                        Encoder::new(SampleRate::Hz48000, value as u32, self.application.clone());
 
                     match new_decoder {
                         Ok(decoder) => self.decoder = decoder,
-                        Err(err) => self.errors.push(format!("OpusDecoder: Error: {err}")),
+                        Err(err) => self.errors.push(format!("OpusDecoder: Error: {err:?}")),
                     }
                     match new_encoder {
                         Ok(encoder) => self.encoder = encoder,
-                        Err(err) => self.errors.push(format!("OpusEncoder: Error: {err}")),
+                        Err(err) => self.errors.push(format!("OpusEncoder: Error: {err:?}")),
                     }
 
-                    self.channels = channels;
+                    self.channels = value as u32;
                     self.setup_buffers();
                     self.setup();
                 }
@@ -174,12 +156,9 @@ impl Codec for CodecOpus {
 
     fn encode(&mut self, data: &mut Vec<f32>) -> Vec<u8> {
         let mut buffer = Vec::new();
-        let chunk = (self.sample_rate as usize
-            * match self.channels {
-                opus::Channels::Mono => 1,
-                opus::Channels::Stereo => 2,
-            }
-            / 1000)
+        let chunk = (48 as usize
+            * self.channels as usize
+            )
             * 20 //ms
         ;
         let mut size;
@@ -214,14 +193,9 @@ impl Codec for CodecOpus {
                 .decode_float(&data, &mut self.input_buffer, false)
             {
                 // TODO: I don't know why this works!
-                Ok(len) => buffer.append(
-                    &mut self.input_buffer[0..len
-                        * match self.channels {
-                            opus::Channels::Mono => 1,
-                            opus::Channels::Stereo => 2,
-                        }]
-                        .to_vec(),
-                ),
+                Ok(len) => {
+                    buffer.append(&mut self.input_buffer[0..len * self.channels as usize].to_vec())
+                }
                 Err(err) => {
                     self.errors
                         .push(format!("OpusDecoder decoding error: {err:?}"));
